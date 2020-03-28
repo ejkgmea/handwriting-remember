4dbba632948dbf5bcd7f1477a934a63859c8e838
sky <linch925@gmail.com>
2020-03-29 02:51:40 +0800

    delete slideMenu.vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/recommended", "eslint:recommended", "@vue/standard"],
+  extends: ['plugin:vue/recommended', 'eslint:recommended', '@vue/standard'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
-};
+}
